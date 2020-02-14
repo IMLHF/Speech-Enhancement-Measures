15071202b905ca0b76a4111eb1910e8d3f515f76
IMLHF <red_wind@foxmail.com>
2020-02-15 02:04:55 +0800

    update evaluate_all.m

--- evaluate_all.m
+++ evaluate_all.m
@@ -3,42 +3,45 @@
 function [Csig, Cbak, Cvol, pesq, SNR, SegSNR] = evaluate_all(ref_dir, deg_dir)
     ref_folder = dir(fullfile(ref_dir,'*.wav'));
     deg_folder = dir(fullfile(deg_dir,'*.wav'));
-    ref_names = [ref_folder.name];
-    deg_names = [deg_folder.name];
-    ref_names = sort(ref_names)
-    deg_names = sort(deg_names)
-    disp(ref_names(1:5))
-    disp(deg_names(1:5))
-    n_refs = length(ref_names)
-    n_degs = length(deg_names)
-    assert(n_refs == n_degs, 'n_refs != n_degs')
-    csigs = zeros(n_refs)
-    cbaks = zeros(n_refs)
-    cvols = zeros(n_refs)
-    pesqs = zeros(n_refs)
-    snrs = zeros(n_refs)
-    segsnrs = zeros(n_refs)
+    ref_names = {ref_folder.name};
+    deg_names = {deg_folder.name};
+    ref_names = sort(ref_names);
+    deg_names = sort(deg_names);
+    disp(ref_names(1:5));
+    disp(deg_names(1:5));
+    n_refs = length(ref_names);
+    n_degs = length(deg_names);
+    assert(n_refs == n_degs, 'n_refs != n_degs');
+    csigs = zeros(1, n_refs);
+    cbaks = zeros(1, n_refs);
+    cvols = zeros(1, n_refs);
+    pesqs = zeros(1, n_refs);
+    snrs = zeros(1, n_refs);
+    segsnrs = zeros(n_refs);
     for idx = 1:n_refs
-        ref_names(idx) = strcat(ref_dir, '/', ref_names(idx))
-        deg_names(idx) = strcat(deg_dir, '/', deg_names(idx))
-        [csig, cbak, cvol] = composite(ref_names(idx), deg_names(idx))
-        [pesq, pesq_2] = pesq(ref_names(idx), deg_names(idx))
-        [snr, segsnr] = comp_snr(ref_names(idx), deg_names(idx))
-        csigs(idx) = csig
-        cbaks(idx) = cbak
-        cvols(idx) = cvol
-        pesqs(idx) = pesq
-        snrs(idx) = snr
-        segsnrs(idx) = segsnr
-        disp(idx)
-    end
-    disp(ref_names(1))
-    disp(deg_names(1))
+        ref_names(idx) = strcat(ref_dir, '/', ref_names(idx));
+        deg_names(idx) = strcat(deg_dir, '/', deg_names(idx));
+        % disp(ref_names(idx));
+        % disp(deg_names(idx));
+        ref_file = char(ref_names(idx));
+        deg_file = char(deg_names(idx));
+        [csig, cbak, cvol] = composite(ref_file, deg_file);
+        pesq_ = comp_pesq(ref_file, deg_file);
+        [snr, segsnr] = comp_snr(ref_file, deg_file);
+        csigs(idx) = csig;
+        cbaks(idx) = cbak;
+        cvols(idx) = cvol;
+        pesqs(idx) = pesq_(1);
+        snrs(idx) = snr;
+        segsnrs(idx) = segsnr;
+        % disp(strcat(ref_names(idx),'\n'))
+        fprintf('\n idx=%d  csig=%f   cbak=%f   cvol=%f    ssnr=%f\n',idx,csig,cbak,cvol,segsnr);
+    end;
 
-    Csig = mean(csigs)
-    Cbak = mean(cbaks)
-    Cvol = mean(cvols)
-    pesq = mean(pesqs)
-    SNR = mean(snrs)
-    SegSNR = mean(segsnrs) 
+    Csig = mean(csigs);
+    Cbak = mean(cbaks);
+    Cvol = mean(cvols);
+    pesq = mean(pesqs);
+    SNR = mean(snrs);
+    SegSNR = mean(segsnrs); 
 end
