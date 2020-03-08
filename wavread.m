e9abf8ca2fc90b72cffb1b12924869385246a42b
IMLHF <red_wind@foxmail.com>
2020-03-08 18:36:15 +0800

    update wavread

--- wavread.m
+++ wavread.m
@@ -2,3 +2,4 @@ function [ ref_data, ref_sampling_rate, nbits ] = wavread( ref_wav )
 	[ ref_data, ref_sampling_rate ] = audioread( ref_wav );
 	info = audioinfo(ref_wav);
 	nbits = info.BitsPerSample;
+end
